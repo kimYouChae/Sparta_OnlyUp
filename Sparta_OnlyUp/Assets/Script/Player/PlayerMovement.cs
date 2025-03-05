fec9fb8fdaf98ce51e004b4fac8050569a57cdc0
김지현 <kjhstar2486@naver.com>
2025-03-05 12:55:38 +0900

    [feat]InputAction 스키마 없이 추가, 동작

--- Sparta_OnlyUp/Assets/Script/Player/PlayerMovement.cs
+++ Sparta_OnlyUp/Assets/Script/Player/PlayerMovement.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("===Move===")]
+    [SerializeField] private Vector2 moveVector;
+    [SerializeField] private Rigidbody playerRb;
+    [SerializeField] private float speed = 3f;
+
+    private void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        PlayerMove();
     }
+
+    private void PlayerMove() 
+    {
+        Vector3 dir = transform.forward * moveVector.y + transform.right * moveVector.x;
+        dir *= speed;
+        playerRb.velocity = dir;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        // 입력이 있으면 받아오기
+        if (context.phase == InputActionPhase.Performed)
+        {
+            moveVector = context.ReadValue<Vector2>();
+        }
+        // 없으면 0 으로 
+        else if(context.phase == InputActionPhase.Canceled) 
+        {
+            moveVector = Vector2.zero;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context) 
+    { 
+    
+    }
+
 }
