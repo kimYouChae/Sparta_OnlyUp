96b0d5cc3e1ab23aead41dabfd3924bddbfb5fd1
김지현 <kjhstar2486@naver.com>
2025-03-06 13:08:29 +0900

    [feat]점프 패널 스크립트 추가

---
+++ Sparta_OnlyUp/Assets/Script/MapObject/JumpPanel.cs
@@ -0,0 +1,27 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPanel : MonoBehaviour
+{
+    [SerializeField]
+    private float jumpPower;
+
+    private void Start()
+    {
+        jumpPower = 15f;
+    }
+
+    // 밟으면 addforce하는 점프대
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent<PlayerManager>(out PlayerManager pl )) 
+        {
+            try { pl.PlayerMovement.Jump(jumpPower); }
+            catch (Exception e) { Debug.Log(e.ToString()); }
+        }
+    }
+}
